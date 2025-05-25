c9ee00d183ed099486440feef0e75f7d64e3e535
zmandyhe <mandy2.he@gmail.com>
2025-05-24 18:29:39 -0700

    update .py

--- prompt-template.py
+++ prompt-template.py
@@ -1,13 +1,17 @@
 import streamlit as st
 from langchain.memory import ConversationBufferMemory
-from langchain_core.prompts.prompt import PromptTemplate
-from langchain.chains.llm import LLMChain
+from langchain_core.prompts import PromptTemplate
+# from langchain.chains.llm import LLMChain
+# from langchain.chains import LLMChain
 # from langchain.llms import HuggingFaceHub
 from langchain_huggingface import HuggingFaceEndpoint
-from langchain.chains.sequential import SequentialChain
+# from langchain.chains.sequential import SequentialChain
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+# from langchain.schema import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
 
 st.title('First App based on LangChain')
-display_text = st.text_input("Type a topic/person to get information")
+display_text = st.text_input("Type a person to get information")
 
 
 # memory
@@ -15,10 +19,23 @@ person_memory = ConversationBufferMemory(input_key='name', memory_key='chat_hist
 match_memory = ConversationBufferMemory(input_key='person', memory_key='chat_history')
 
 # Prompt Templates
-first_input_prompt = PromptTemplate(
-    input_variables = ['name'],
-    template = "Tell me something about celebrity {name}"
-)
+# first_input_prompt = PromptTemplate.from_template(
+    # input_variables = ['name'],
+    # template = "Tell me something about celebrity {name}"
+# )
+# Prompt Templates
+first_input_prompt = PromptTemplate.from_template("Tell me something about celebrity {name}")
+
+
+# Step 2: Create a prompt template
+# prompt = PromptTemplate(
+    # input_variables=["chat_history", "user_input"],
+    # template="""
+    # The following is a conversation:
+    # {chat_history}
+    # User: {user_input}
+    # AI:"""
+# )
 
 # HuggingFaceHub LLMs
 llm = HuggingFaceEndpoint(
@@ -28,38 +45,51 @@ llm = HuggingFaceEndpoint(
 )
 
 
-chain = LLMChain(
-    llm=llm,
-    prompt=first_input_prompt,
-    verbose=True,
-    output_key='person',
-    memory=person_memory
-)
+# chain = LLMChain(
+    # llm=llm,
+    # prompt=first_input_prompt,
+    # verbose=True,
+    # output_key='person',
+    # memory=person_memory
+# )
 
 # Prompt Templates
-second_input_prompt=PromptTemplate(
-    input_variables=['person'],
-    template="Tell 3 major matches of {person} career"
-)
+# second_input_prompt=PromptTemplate(
+    # input_variables=['person'],
+    # template="Tell 3 major matches of {person} career"
+# )
+# Prompt Templates
+second_input_prompt=PromptTemplate.from_template("Tell 3 major matches of {person} career")
 
-chain2=LLMChain(
-    llm=llm, prompt=second_input_prompt,
-    verbose=True, output_key='matches',
-    memory=match_memory
-)
 
-parent_chain = SequentialChain(
-    chains=[chain, chain2],
-    input_variables = ['name'],
-    output_variables=['person', 'matches'],
-    verbose=True
-)
+# chain2=LLMChain(
+    # llm=llm, prompt=second_input_prompt,
+    # verbose=True, output_key='matches',
+    # memory=match_memory
+# )
+
+# parent_chain = SequentialChain(
+    # chains=[chain, chain2],
+    # input_variables = ['name'],
+    # output_variables=['person', 'matches'],
+    # verbose=True
+# )
+
+first_chain = first_input_prompt | llm | StrOutputParser()
+second_chain = second_input_prompt | llm | StrOutputParser()
+# optimization_chain = optimization_prompt | llm | StrOutputParser()
+
+
+chain = ({"structure" : first_chain} 
+        | RunnablePassthrough.assign(review=second_chain))
 
 if display_text:
-    st.write(parent_chain({'name': display_text}))
+    st.write(first_chain({'name': display_text}))
 
     with st.expander('Person Name'):
         st.info(person_memory.buffer)
 
     with st.expander('Major Matches'):
         st.info(match_memory.buffer)
+
+
